9432740e1da0c73cd815696272f6568273a7d327
MURAKI Taichi <muraki.taichi@gmail.com>
2019-11-12 18:51:26 +0900

    Fix to add security group and ec2 instance.

--- bin/main.ts
+++ bin/main.ts
@@ -6,8 +6,30 @@ import ec2 = require('@aws-cdk/aws-ec2');
 export class MyEc2Stack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-        // The code that defines your stack goes here
-        const vpc = new ec2.Vpc(this, 'VPC');
+        const vpc = new ec2.Vpc(this, 'VpcForGatling', {
+            maxAzs: 1,
+            subnetConfiguration: [
+                {
+                  subnetType: ec2.SubnetType.PUBLIC,
+                  name: 'Ingress'
+                }
+            ]
+        });
+        const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+            vpc,
+            description: 'Allow all outbound from ec2 instances',
+            allowAllOutbound: true
+        });
+        let ec2Instance = new ec2.CfnInstance(this, 'myInstance', {
+            imageId: new ec2.AmazonLinuxImage().getImage(this).imageId,
+            instanceType: new ec2.InstanceType('t3.small').toString(),
+            networkInterfaces: [{
+              associatePublicIpAddress: true,
+              deviceIndex: '0',
+              groupSet: [securityGroup.securityGroupId],
+              subnetId: vpc.publicSubnets[0].subnetId
+            }]
+        });
     }
   }
 
